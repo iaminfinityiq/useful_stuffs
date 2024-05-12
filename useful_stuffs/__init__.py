8a7477855b866404939316e2efc9445dd6684624
iaminfinityiq <133767092+iaminfinityiq@users.noreply.github.com>
2024-05-12 13:58:05 +0700

    __init__.py

--- useful_stuffs/__init__.py
+++ useful_stuffs/__init__.py
@@ -2,25 +2,25 @@
 Helps you in daily "programming" life
 """
 def include(module: str) -> None:
-    """
-    Includes all modules in useful_stuffs needed.
-    """
-    match module:
-        case "math":
-            import useful_stuffs.Math
-        case "io":
-            import useful_stuffs.IO
-        case "input/output":
-            import useful_stuffs.IO
-        case "error":
-            import useful_stuffs.Errors
-        case _:
-            import useful_stuffs.IO
-            import useful_stuffs.Errors
-            if type(module) is not str:
-                raise useful_stuffs.Errors.NoTypeMatchedError(f"module requires str, not {useful_stuffs.IO.return_type(module)}")
-            else:
-                raise useful_stuffs.Errors.NoValueMatchedError(f"""No inputs defined {module.lower()} in useful_stuffs
+  """
+  Includes all modules in useful_stuffs needed.
+  """
+  match module.lower():
+      case "math":
+          import useful_stuffs.Math
+      case "io":
+          import useful_stuffs.IO
+      case "input/output":
+          import useful_stuffs.IO
+      case "error":
+          import useful_stuffs.Errors
+      case _:
+          import useful_stuffs.IO
+          import useful_stuffs.Errors
+          if type(module) is not str:
+              raise useful_stuffs.Errors.NoTypeMatchedError(f"module requires str, not {useful_stuffs.IO.return_type(module)}")
+          else:
+              raise useful_stuffs.Errors.NoValueMatchedError(f"""No inputs defined {module.lower()} in useful_stuffs
 All inputs defined available in useful_stuffs are as followings:
 1. math: imports useful_stuffs.Math
 2. io: imports useful_stuffs.IO
