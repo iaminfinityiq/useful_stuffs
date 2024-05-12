961d2422b68229c30f8f7ddba67fbe3a3710e808
iaminfinityiq <133767092+iaminfinityiq@users.noreply.github.com>
2024-05-12 14:00:40 +0700

    __init__.py

--- useful_stuffs/Math/__init__.py
+++ useful_stuffs/Math/__init__.py
@@ -1,26 +1,27 @@
 """
 Includes all of the functions you need for Math
 """
+
 def include(module: str):
-    """
-    Includes all modules in useful_stuffs.Math needed
-    """
-    match module.lower():
-        case "calculation":
-            import useful_stuffs.Math.Calculations
-        case "equation":
-            import useful_stuffs.Math.Calculations
-        case "formula":
-            import useful_stuffs.Math.Calculations
-        case "conversion":
-            import useful_stuffs.Math.Conversions
-        case _:
-            import useful_stuffs.Errors
-            import useful_stuffs.IO
-            if type(module) is not str:
-                raise useful_stuffs.Errors.NoTypeMatchedError(f"""module requires str, not {useful_stuffs.IO.return_type(module)}""")
-            else:
-                raise useful_stuffs.Errors.NoValueMatchedError(f"""No inputs defined {module.lower()} in useful_stuffs.Math
+  """
+  Includes all modules in useful_stuffs.Math needed
+  """
+  match module.lower():
+    case "calculation":
+      import useful_stuffs.Math.Calculations
+    case "equation":
+      import useful_stuffs.Math.Calculations
+    case "formula":
+      import useful_stuffs.Math.Calculations
+    case "conversion":
+      import useful_stuffs.Math.Conversions
+    case _:
+      import useful_stuffs.Errors
+      import useful_stuffs.IO
+      if type(module) is not str:
+        raise useful_stuffs.Errors.NoTypeMatchedError(f"""module requires str, not {useful_stuffs.IO.return_type(module)}""")
+      else:
+        raise useful_stuffs.Errors.NoValueMatchedError(f"""No inputs defined {module.lower()} in useful_stuffs.Math
 All of the inputs defined available in useful_stuffs.Math are as followings:
 1. calculation/equation imports useful_stuffs.Math.Calculations
 2. conversion imports useful_stuffs.Math.Conversions
