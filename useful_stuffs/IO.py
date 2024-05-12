7de9c4a77fe498ee08f2ce7e8dcab6f06104ac90
iaminfinityiq <133767092+iaminfinityiq@users.noreply.github.com>
2024-05-12 13:59:06 +0700

    IO.py

--- useful_stuffs/IO.py
+++ useful_stuffs/IO.py
@@ -1,16 +1,18 @@
 """
 Includes all of the functions you need for inputs and outputs.
 """
-def return_type(value) -> str:
-    """
-    Helps you get the neater value of the type. For example: return_type("Hello World") returns str
-    """
-    string_type = str(type(value))
-    type_ = ""
-    for i, c in enumerate(string_type):
-        if i < len("<class '") or i >= len(string_type) - len("'>"):
-            continue
+import useful_stuffs
 
-        type_ += c
-    
-    return type_
+def return_type(value) -> str:
+  """
+  Helps you get the neater value of the type. For example: return_type("Hello World") returns str
+  """
+  string_type = str(type(value))
+  type_ = ""
+  for i, c in enumerate(string_type):
+    if i < len("<class '") or i >= len(string_type) - len("'>"):
+      continue
+
+    type_ += c
+
+  return type_
